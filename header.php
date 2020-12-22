a72fb87188baa3959e897889f89c73dbbaa07592
Josh Dodd <joshuatdodd@gmail.com>
2020-12-22 10:58:49 -0500

    header theme name update

--- header.php
+++ header.php
@@ -66,22 +66,22 @@ if(in_array($GLOBALS['templateName'], array('latest','about','galleries','contac
 <link href="<?=$GLOBALS['path']?>style.css" rel="stylesheet" type="text/css" />
 <?php print '<script type="text/javascript">var path = "' . $GLOBALS['path'] . '";var url = "' . $GLOBALS['url'] . '";var fullurl = "' . get_permalink() . '"</script>'; ?>
 
-<link rel="apple-touch-icon" sizes="57x57" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/favicon-16x16.png">
-<link rel="manifest" href="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/manifest.json">
+<link rel="apple-touch-icon" sizes="57x57" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/favicon-16x16.png">
+<link rel="manifest" href="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/manifest.json">
 <meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="<?=$GLOBALS['url']?>/wp-content/themes/apic/images/favicon/ms-icon-144x144.png">
+<meta name="msapplication-TileImage" content="<?=$GLOBALS['url']?>/wp-content/themes/Industry-Perspectives/images/favicon/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
 
 
